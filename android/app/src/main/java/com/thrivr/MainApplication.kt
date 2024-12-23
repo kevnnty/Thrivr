b3da82ad1fced4e2fa8d26ad732f10e296df83ca
Kevin TUYIZERE <122741116+Kevin-Tyy@users.noreply.github.com>
2024-12-24 00:44:36 +0200

    chore: update project name

--- android/app/src/main/java/com/thrivr/MainApplication.kt
+++ android/app/src/main/java/com/thrivr/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.barernc
+package com.thrivr
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/ThrivrTests/ThrivrTests.m
+++ ios/ThrivrTests/ThrivrTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface BareRNCTests : XCTestCase
+@interface ThrivrTests : XCTestCase
 
 @end
 
-@implementation BareRNCTests
+@implementation ThrivrTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
